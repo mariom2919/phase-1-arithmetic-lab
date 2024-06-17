9644c26dde0671cf5d06e6e787ac7a1ada5fba45
+ <+>
2024-06-17 00:26:46 -0700

    -m added equations

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+let multiply = (num1 * num2)
+
+
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;  
+
+
+
+let num3 = 12
+let num4 = 3
+
+let mod = (num3 / num4)
+
+
+
+const mySet = new Set([3, 7, 2, 20, 5]);
+
+const max = Math.max(...mySet);
+
+console.log(highestNumber); 
